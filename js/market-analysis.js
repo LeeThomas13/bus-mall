8e4c0aa1a4e8ea5758fca7d4dbf94d60fa531a39
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-19 21:08:16 -0700

    got random unique images to show up, fiddling with radio button. Still doesnt work

--- js/market-analysis.js
+++ js/market-analysis.js
@@ -2,14 +2,18 @@
 
 var productArray = [];
 var uniqueProductArray = [];
-var parentElement = document.getElementById('productTest')
-var results = document.getElementById('results')
+var parentElement = document.getElementById('productTest');
+var results = document.getElementById('results');
+var button = document.getElementById('input');
 var totalClicks = 0;
+var maxClicks = 3;
 
 function Product (name, filepath) {
   this.name=name;
   this.filepath=filepath;
   productArray.push(this);
+  this.clicks = 0;
+  this.shown = 0;
 }
 
 new Product('bag', '../images/bag.jpg')
@@ -32,43 +36,75 @@ new Product('unicorn', '../images/unicorn.jpg')
 new Product('usb', '../images/usb.gif')
 new Product('water-can', '../images/water-can.jpg')
 new Product('wine-glass', '../images/wine-glass.jpg')
-console.log(productArray)
 
 
 function generateRandomImage(){
   var randomImage = getRandomNumber(productArray.length);
+
+  while(uniqueProductArray.includes(randomImage)){
+    randomImage = getRandomNumber(productArray.length);
+  }
+  uniqueProductArray.push(randomImage);
   var chosenImage = productArray[randomImage];
+  chosenImage.shown++;
+
+  if(uniqueProductArray.length>6){
+    uniqueProductArray.shift();
+  }
+  buildImage(chosenImage)
+}
+
+function buildImage(chosenImage){
   var image = document.createElement('img');
   image.setAttribute('src', chosenImage.filepath);
   image.setAttribute('alt', chosenImage.name);
   image.setAttribute('title', chosenImage.name);
-  parentElement.appendChild(image)
+  
+  var radioButton = document.createElement('input');
+  radioButton.setAttribute('type', 'radio');
+  radioButton.setAttribute('name', chosenImage.name);
+  radioButton.setAttribute('src', chosenImage.filepath);
+  parentElement.appendChild(radioButton);
+  parentElement.appendChild(image);
+
+}
+
+function handleClick(event){
+  totalClicks++
+
+  var name = event.target.value;
+  for(var i=0;i<productArray.length;i++){
+    if(name === productArray[i].name){
+      productArray[i].clicks++;
+    }
+  }
+
+  parentElement.innerHTML='';
+  generateRandomImage();
+  generateRandomImage();
+  generateRandomImage();
+  if(totalClicks>=maxClicks){
+    parentElement.removeEventListener('click', handleClick);
+    parentElement.innerHTML='';
+    button.innerHTML='';
+    generateResults();
+  }
+}
+
+function generateResults(){
+  for(var i=0;i<productArray.length;i++) {
+    var li = document.createElement('li');
+    li.textContent = `${productArray[i].name} had ${productArray[i].clicks} votes and was shown ${productArray[i].shown} times.`
+    results.appendChild(li)
+  }
 }
 
 function getRandomNumber(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function handleClick(event){
-  totalClicks++;
-  var event = event.target.value
-
-  for(var i=0; i<productArray.length; i++){
-    if(event===productArray[i].event){
-      productArray[i].totalClicks++
-    }
-  }
-  parentElement.innerHTML='';
-  generateRandomImage();
-  generateRandomImage();
-  generateRandomImage();
-  if(totalClicks>=25){
-    parentElement.removeEventListener('click', handleClick);
-  }
-}
-
-parentElement.addEventListener('click', handleClick);
-
 generateRandomImage()
 generateRandomImage()
 generateRandomImage()
+
+document.getElementById('input').addEventListener('click', handleClick)
